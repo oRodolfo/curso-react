a3be89fa2dba66cf8b9be45a5a298c207d6dc2f7
oRodolfo <rodolforib.dev@hotmail.com>
2026-03-22 20:54:46 -0300

    aprendendo na pratica e a teoria dos fundamentos de react

---
+++ src/components/Tasks.jsx
@@ -0,0 +1,40 @@
+import { ChevronRightIcon, TrashIcon } from "lucide-react";
+import { useNavigate } from "react-router-dom";
+import Button from "./Button";
+
+function Tasks({ tasks, onTaskClick, onDeleteTaskClick }) {
+  const navigation = useNavigate();
+
+  function onSeeDatailsClick(task) {
+    const query = new URLSearchParams({
+      title: task.title,
+      description: task.description,
+    }).toString();
+    navigation(`/task?${query.toString()}`);
+  }
+  return (
+    <ul className="space-y-4 p-6 bg-slate-200 rounded-md shadow">
+      {tasks.map((task) => (
+        <li key={task.id} className="flex gap-2">
+          <button
+            onClick={() => onTaskClick(task.id)}
+            className={
+              "bg-slate-400 text-left w-full text-white p-2 rounded-md " +
+              (task.isCompleted ? "line-through" : "")
+            }
+          >
+            {task.title}
+          </button>
+          <Button onClick={() => onSeeDatailsClick(task)}>
+            <ChevronRightIcon />
+          </Button>
+
+          <Button onClick={() => onDeleteTaskClick(task.id)}>
+            <TrashIcon />
+          </Button>
+        </li>
+      ))}
+    </ul>
+  );
+}
+export default Tasks;
