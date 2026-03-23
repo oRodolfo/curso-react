57ef92424b7c2f5890c5557eb41aedf524d25c05
oRodolfo <rodolforib.dev@hotmail.com>
2026-03-22 21:03:27 -0300

    adicionando o check para tarefas concluidas

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,4 +1,4 @@
-import { ChevronRightIcon, TrashIcon } from "lucide-react";
+import { ChevronRightIcon, TrashIcon, CheckIcon } from "lucide-react";
 import { useNavigate } from "react-router-dom";
 import Button from "./Button";
 
@@ -23,7 +23,7 @@ function Tasks({ tasks, onTaskClick, onDeleteTaskClick }) {
               (task.isCompleted ? "line-through" : "")
             }
           >
-            {task.title}
+            {task.isCompleted && <CheckIcon />} {task.title}
           </button>
           <Button onClick={() => onSeeDatailsClick(task)}>
             <ChevronRightIcon />
